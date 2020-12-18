0284b73a698e39b90330c1e6337e6c6c36a51f1f
Lu Khei <kheiboardwarrior@gmail.com>
2020-12-18 11:43:42 +0800

    task 6 complete clear button share button

--- frontend/src/app/components/main.component.ts
+++ frontend/src/app/components/main.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {CameraService} from '../camera.service';
 
 @Component({
@@ -9,17 +11,33 @@ import {CameraService} from '../camera.service';
 export class MainComponent implements OnInit {
 
 	imagePath = '/assets/cactus.png'
-
-	constructor(private cameraSvc: CameraService) { }
+	form : FormGroup
+	constructor(private cameraSvc: CameraService, private fb: FormBuilder, private http: HttpClient) { }
 
 	ngOnInit(): void {
+		this.form = this.fb.group({
+			image: this.fb.control('', Validators.required),
+			title: this.fb.control('', Validators.required),
+			comments: this.fb.control('', Validators.required)
+		})
 	  if (this.cameraSvc.hasImage()) {
 		  const img = this.cameraSvc.getImage()
 		  this.imagePath = img.imageAsDataUrl
+		  this.form.get('image').setValue(this.imagePath)
 	  }
+	
 	}
 
 	clear() {
 		this.imagePath = '/assets/cactus.png'
+	}
+
+	share(){
+		const formData = new FormData()
+		console.log(this.form.get('image').value)
+		formData.set('image',this.form.get('image').value)
+		formData.set('title', this.form.get('title').value)
+		formData.set('comments',this.form.get('comments').value )
+		
 	}
 }
