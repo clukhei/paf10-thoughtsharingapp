606f2e46df095c7bc04199d355859e0e62faa92e
Lu Khei <kheiboardwarrior@gmail.com>
2020-12-21 12:20:17 +0800

    add authentication before comments submission

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,12 +1,28 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http"
-import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+
 
 @Injectable()
 export class AuthService {
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) { }
+
+    credentials
 
     authLogin(credentials): Promise<any> {
-        return this.http.post('/login/', credentials ).toPromise()
+        this.credentials = credentials
+        return this.http.post('/login/', credentials).toPromise()
+    }
+
+    async authAndSubmit(formData): Promise<any> {
+        try {
+            const authResult = await this.http.post('/login/', this.credentials).toPromise()
+            console.log(authResult)
+            const postComments = await this.http.post('/share/', formData).toPromise()
+
+        } catch (e) {
+            console.log(e)
+            throw e
+        }
+
     }
 }
--- frontend/src/app/components/main.component.ts
+++ frontend/src/app/components/main.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 import {CameraService} from '../camera.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class MainComponent implements OnInit {
 	imagePath = '/assets/cactus.png'
 	form : FormGroup
 	imgLinkRef
-	constructor(private cameraSvc: CameraService, private fb: FormBuilder, private http: HttpClient, private router: Router) { }
+	constructor(private authSvc: AuthService,private cameraSvc: CameraService, private fb: FormBuilder, private http: HttpClient, private router: Router) { }
 
 	ngOnInit(): void {
 		this.form = this.fb.group({
@@ -40,8 +41,7 @@ export class MainComponent implements OnInit {
 		formData.set('image',this.form.get('image').value)
 		formData.set('title', this.form.get('title').value)
 		formData.set('comments',this.form.get('comments').value )
-		this.http.post('/share/', formData)
-			.toPromise()
+		this.authSvc.authAndSubmit(formData)
 			.then(()=> {
 				this.form.reset()
 				this.imagePath = '/assets/cactus.png'
@@ -49,7 +49,10 @@ export class MainComponent implements OnInit {
 			.catch(err=> {
 		
 				if(err.status == 401){
+					this.form.reset()
+					this.imagePath = '/assets/cactus.png'
 					this.router.navigate(['/'])
+					
 				}
 				console.log(err)
 			})
